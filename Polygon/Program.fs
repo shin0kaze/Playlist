40bf2046951e3f4be148fa7ada1844ab40582898
shin0kaze <Kirill.moskva@mail.ru>
2024-04-28 20:06:07 -0600

    Добавил подключение к базе данных

---
+++ Polygon/Program.fs
@@ -0,0 +1,12 @@
+﻿
+open Microsoft.Data.Sqlite
+
+[<Literal>]
+let connectionString = 
+    "Data Source=" + 
+    __SOURCE_DIRECTORY__ + @"/../../../tests/SqlProvider.Tests/scripts/northwindEF.db;" + 
+    "Version=3;foreign keys=true"
+
+let connection = new SqliteConnection("Data Source=db.db")
+connection.Open()
+    
